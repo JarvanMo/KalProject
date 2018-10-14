b70aed5bc0950e73decb63d6a1b5a86a6342dabe
JarvanMo <jarvan.mo@gmail.com>
2018-10-14 16:23:22 +0800

    update

--- kal/src/main/java/com/jarvanmo/kal/KalExtensions.kt
+++ kal/src/main/java/com/jarvanmo/kal/KalExtensions.kt
@@ -1,9 +1,16 @@
 package com.jarvanmo.kal
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.view.View
 import com.jarvanmo.kal.util.MToast
 import com.jarvanmo.kal.util.MToast.TYPE_INFO
 import com.orhanobut.logger.Logger
+import java.io.File
+import java.io.BufferedOutputStream
+import java.io.FileOutputStream
+import java.util.*
+
 
 /**
  * extensions for {@link View}
@@ -61,6 +68,35 @@ fun Int.toast(@MToast.ToastType toastType: Int = TYPE_INFO) {
     MToast.show(this,toastType)
 }
 
+
+/***
+ * extension for bitmap
+ */
+/****
+ * @param context if des not provided ,then context must be not null
+ */
+fun Bitmap.save(context:Context?=null,des: File? =null,compressFormat: Bitmap.CompressFormat=Bitmap.CompressFormat.PNG,quality:Int=100):File?{
+    if (context == null&& des == null){
+        throw IllegalArgumentException("context and des can't both be  NULL")
+    }
+    val file = if(des==null){
+        des
+    }else{
+
+        val fileName = UUID.randomUUID().toString() + when (compressFormat) {
+            Bitmap.CompressFormat.WEBP -> ".webp"
+            Bitmap.CompressFormat.PNG -> ".png"
+            else -> ".jpeg"
+        }
+        File(context!!.cacheDir,fileName)
+    }
+    val bos = BufferedOutputStream(FileOutputStream(des))
+    this.compress(compressFormat, quality, bos)
+    bos.flush()
+    bos.close()
+
+    return file
+}
 
 
 
